4e09e09f464800cda83cdcf05e71b308ecb58089
刘宁 <liuhnnn@163.com>
2021-11-30 10:15:04 +0800

    🐞 fix: 修复app未传入的bug

--- src/boot/register.js
+++ src/boot/register.js
@@ -1,6 +1,6 @@
 import HasPermission from "../component/has-permission.js";
 
-export default ({ store }) => {
+export default ({ app, store }) => {
   // 'has-permission' will be used as 'v-has-permission'
   app.directive("has-permission", HasPermission(store));
 };
